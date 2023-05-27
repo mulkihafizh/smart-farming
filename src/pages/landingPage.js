32210934ccd089287e2fd33e72cabbbd9374a280
gloeserin <erinafebrianti@smkwikrama.sch.id>
2023-05-27 19:55:19 +0700

    Complete landing page

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -2,7 +2,11 @@ import React from "react";
 import "../assets/css/landing.css";
 import { Link } from "react-router-dom";
 
+
 export default function landingPage() {
+
+
+
   return (
     <div id="landing">
       <header className="landingHeader">
@@ -32,7 +36,102 @@ export default function landingPage() {
           </div>
         </div>
       </header>
-    
+
+      <div className="card-container">
+        <h1 className="card-heading">For your Information</h1>
+        <div className="card-content">
+          <div className="card">
+          <span class="tag">Feature</span>
+            <h2 className="card-title">Title</h2>
+            <div className="card-info">
+            <p className="info">Elemenatary tracks all the events for the day as you scheduled and you will never have to worry.</p>
+            </div>
+          </div>
+          <div className="card">
+          <span class="tag">Feature</span>
+            <h2 className="card-title">Title</h2>
+            <div className="card-info">
+              <p className="info">Elemenatary tracks all the events for the day as you scheduled and you will never have to worry.</p>
+            </div>
+          </div>
+          <div className="card">
+          <span class="tag">Feature</span>
+            <h2 className="card-title">Title</h2>
+            <div className="card-info">
+            <p className="info">Elemenatary tracks all the events for the day as you scheduled and you will never have to worry.</p>
+            </div>
+          </div>        
+        </div>
+      </div>
+
+      <div className="footer">
+        <div className="sb__footer section-padding">
+          <div className="sb__footer-link">
+            <div className="sb__footer-link-div">
+              <h4>For Business</h4>
+              <a href="/">
+                <p>Employer</p>
+              </a>
+              <a href="/">
+                <p>Health plan</p>
+              </a>
+              <a href="/">
+                <p>Individual</p>
+              </a>
+            </div>
+            <div className="sb__footer-link-div">
+              <h4>Head Office</h4>
+              <a href="/">
+                <p>
+                  Jl. Pelajar Pejuang 45 No.65,Lkr. Sel.,Lengkong,Kota Bandung,Jawa Barat 40263
+                  Lkr. Sel., Kec. Lengkong, Kota Bandung, Jawa Barat 40264
+                </p>
+              </a>
+            </div>
+            <div className="sb__footer-link-div">
+              <h4>Contact</h4>
+              <a href="/">
+                <p>
+                  Nomer Telfon (Kantor Utama): (022) 7351 6650
+                  Email: pt.lskk@gmail.comh
+                </p>
+              </a>
+            </div>
+            <div className="sb__footer-link-div">
+              <h4>Workshop</h4>
+              <a href="/">
+                <p>
+                  Jl. Punclut No. 516 Ciumbeuleuit, Kec. Cidadap, Kota Bandung, Jawa Barat, 40142
+                </p>
+              </a>
+              <a href="/">
+                <p>Nomer Telfon (Workshop): (022) 7302571</p>
+              </a>
+            </div>
+            <div className="sb__footer-link-div">
+              <h4>Our Socialmedia</h4>
+              <div className="socialmedia">
+                <p><i class="fab fa-facebook-f"></i></p>
+                <p><i class="fa-brands fa-instagram"></i></p>
+                <p><i class="fa-brands fa-youtube"></i></p>
+              </div>
+            </div>
+            <hr></hr>
+
+            <div className="sb__footer-below">
+              <div className="sb__footer-copyright">
+                <p>
+                  @{new Date(). getFullYear()} LSKK Smart Farming Tracker
+                </p>
+              </div>
+              <div className="sb__footer-terms">
+                <a href="/"><div><p>Terms & conditions</p></div></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
 
     </div>
   );
