8263f617e0e849ab099e2611c3d9cddf5feefc24
MulkiHafizh <ukiyo142@gmail.com>
2023-05-29 12:24:39 +0700

    login

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -43,13 +43,6 @@ export default function LoginPage() {
           email: email,
           password: password,
         },
-        {
-          withCredentials: true,
-          headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-          },
-        }
       )
       .then((res) => {
         console.log(res);
