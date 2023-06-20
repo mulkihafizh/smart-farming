65004f0539a646cb4466b419c2918bb4cdbd7eae
MulkiHafizh <ukiyo142@gmail.com>
2023-06-20 15:25:14 +0700

    sedikit

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const router = createBrowserRouter(
             {
               withCredentials: true,
               headers: {
-                "Cookie": "token=" + document.cookie.token,
+                Cookie: "token=" + document.cookie.token,
                 "Content-Type": "application/json",
               },
             }
