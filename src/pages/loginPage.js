a63718ea86bbf2528809f3c281a4fc6942646566
MulkiHafizh <ukiyo142@gmail.com>
2023-06-20 14:54:21 +0700

    sedikit

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -53,7 +53,7 @@ export default function LoginPage() {
             path: "/",
             maxAge: 36000,
             sameSite: "none",
-            secure: false,
+            secure: true,
           });
           navigate("/dashboard");
         }
