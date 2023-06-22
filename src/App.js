82f444b40fbf5a00b698d3bd4a85355408fbeb21
MulkiHafizh <ukiyo142@gmail.com>
2023-06-22 14:46:57 +0700

    test

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ const router = createBrowserRouter(
               }
             });
 
-          return;
+          return null;
         }}
         element={<RegisterPage />}
       />
@@ -60,7 +60,7 @@ const router = createBrowserRouter(
               }
             });
 
-          return;
+          return null;
         }}
         element={<LoginPage />}
       />
